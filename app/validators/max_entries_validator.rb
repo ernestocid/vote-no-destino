c313d445be1efd80addf4e86df9dd5b00debb913
Ernesto Cid <ernesto.cid@gocase.com.br>
2017-08-09 21:55:16 +0000

    Remove lost byebug.

--- app/validators/max_entries_validator.rb
+++ app/validators/max_entries_validator.rb
@@ -1,6 +1,5 @@
 class MaxEntriesValidator < ActiveModel::Validator
   def validate(record)
-    byebug
     if record.class.count >= 5
       record.errors.add(:max_entries, 'only five entries allowed')
     end
