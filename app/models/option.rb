20e155f6658fbb016abe220890890529b98eb35e
Ernesto Cid <ernesto.cid@gocase.com.br>
2017-08-11 00:13:01 +0000

    Remove validation temporarely from Option. Add method to build combinations of options. Add test for this method.

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,4 +1,7 @@
 class Option < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates_with MaxEntriesValidator
+
+  def self.build_random_combinations
+    all.to_a.combination(2).to_a.shuffle!
+  end
 end
--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 describe Option, type: :model do
+  before(:each) do
+    create_sample_options
+  end
+
   let(:new_option) { build(:option) }
 
   describe '#name' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
+  end
+
+  describe '.build_random_combinations' do
+    it 'returns an array with random option combinations' do
+      combinations = Option.build_random_combinations
+
+      expect(combinations.class).to be_equal Array
+      expect(combinations.size).to be_equal 10
+    end
   end
 end
