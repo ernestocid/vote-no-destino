806f4732cab4525e90f88b20a96f9bd769d0ef6d
Ernesto Cid <ernesto.cid@gocase.com.br>
2017-08-12 15:34:54 +0000

    Add spec for Option #delete method.

--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -3,13 +3,27 @@ require 'rails_helper'
 describe Option, type: :model do
   before(:each) do
     create_sample_options
+    @sample_option = Option.last
   end
-
-  let(:new_option) { build(:option) }
 
   describe '#name' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
+  end
+
+  describe '#delete' do
+    it 'deletes and returns true if it can be destroyed' do
+      expect(@sample_option.delete).to be_truthy
+    end
+
+    it 'returns false if it can not be destroyed' do
+      sample_survey = Survey.create
+      sample_question = Question.create(survey: sample_survey)
+      sample_question.options << @sample_option
+      sample_question.save
+
+      expect(@sample_option.delete).to be_falsy
+    end
   end
 
   describe '.build_random_combinations' do
